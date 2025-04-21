1c4d7feee26ed5a148ad939af9de25ec45c183f2
FozzzSizTa <ben917355@gmail.com>
2025-04-21 11:29:32 +0800

    feat(product): 利用 DatePipe 顯示建檔日期

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,9 @@ export class AppComponent {
   isShow = true;
 
   photoUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
+
+  createDate = new Date('2025/4/9');
+
   onSetDisplay(isShow: boolean): void {
     this.isShow = isShow;
   }
