17e299d73e3a995c983590dc65c56c76cce93f58
FozzzSizTa <ben917355@gmail.com>
2025-04-28 14:03:55 +0800

    feat(pagination): 新增分頁元件

---
+++ src/app/pagination/pagination.component.ts
@@ -0,0 +1,20 @@
+import { Component, computed, OnInit, signal, input, numberAttribute, output, model } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+
+  standalone: true,
+  imports: [],
+  templateUrl: './pagination.component.html',
+  styleUrl: './pagination.component.scss',
+})
+export class PaginationComponent {
+  readonly totalCount = input.required<number, string | number>({ transform: numberAttribute });
+  readonly pageSize = input.required<number, string | number>({ transform: numberAttribute });
+  readonly pageIndex = model.required();
+
+  readonly range = computed(() => {
+    const length = Math.ceil(this.totalCount() / this.pageSize());
+    return Array.from({ length }, (_, i) => i + 1);
+  });
+}
