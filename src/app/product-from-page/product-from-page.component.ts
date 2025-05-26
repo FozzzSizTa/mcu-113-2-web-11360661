4b90303de69e69c17ccf236c9e24f468333d90f2
FozzzSizTa <ben917355@gmail.com>
2025-05-26 13:40:17 +0800

    feat(product-form): 實作產品作者表單輸入

--- src/app/product-from-page/product-from-page.component.ts
+++ src/app/product-from-page/product-from-page.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute, Data } from '@angular/router';
 import { Product } from '../models/product';
 import { map } from 'rxjs';
 import { JsonPipe } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-from-page',
@@ -18,12 +18,23 @@ export class ProductFromPageComponent implements OnInit {
   form = new FormGroup({
     id: new FormControl<string | null>(null),
     name: new FormControl<string | null>(null),
+    authors: new FormArray<FormControl<string | null>>([]),
     company: new FormControl<string | null>(null),
     price: new FormControl<number | null>(null),
   });
 
   product!: Product;
+
+  get authors(): FormArray<FormControl<string | null>> {
+    return this.form.get('authors') as FormArray<FormControl<string | null>>;
+  }
+
   ngOnInit(): void {
     this.route.data.pipe(map(({ product }: Data) => product)).subscribe((product) => (this.product = product));
+  }
+
+  onAddAuthor(): void {
+    const formControl = new FormControl<string | null>(null);
+    this.authors.push(formControl);
   }
 }
